37f2db70f0352431040e5f8fbbfce813372640b0
DODDSnowflakeBadge1 <151786507+DODDSnowflakeBadge1@users.noreply.github.com>
2023-11-24 11:41:21 -0500

    Update streamlit_app.py
    Presented data from data vice more cleanly in a table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json()) 
+
+# This structures the data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# This presents the data in a table
+streamlit.dataframe(fruityvice_normalized)
