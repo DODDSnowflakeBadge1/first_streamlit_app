8cea389eba2a6819e0e7c831a8cb7971625be23c
DODDSnowflakeBadge1 <151786507+DODDSnowflakeBadge1@users.noreply.github.com>
2023-11-24 14:42:06 -0500

    Update streamlit_app.py
    removed default fruit selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
@@ -38,6 +38,7 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
